15e02265b0ff9273485357b150f83c36a766ba03
Richelle Burke <richelleburke@Richelles-MacBook-Pro.local>
2018-11-09 19:33:58 -0500

    v4: added comments to js

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-// Variables
+// VARIABLES
 
 var letters = ["a", "b", "c", "d", "e",
                 "f", "g", "h", "i", "j",
@@ -20,49 +20,76 @@ var numberLosses = document.getElementById("number-losses");
 var display = document.getElementById("display");
 
 
-// Functions
+// FUNCTIONS
 
+// start or reset the game
 function initialiseGame () {
+
+    // generate new random letter
     randomLetter =  Math.floor(Math.random() * 25);
     letters[randomLetter];
+    // set the guessed letter string to empty
     guessedLetters = "";
+    // reset number of guesses to 10 and display it in the dom
     numGuesses = 10;
     numberGuesses.textContent = numGuesses;
+
+    // tester - DELETE ON DEPLOY
     console.log(letters[randomLetter]);
 }
 
+// check if user input matches
 function matchKey () {
 
+    // creates input variable and coverts to lower case
     var userGuess = event.key.toLocaleLowerCase();
 
+    // if the guessed letter is correct...
     if (userGuess === letters[randomLetter]) {
-        document.getElementById("display").style.color = "#17C800";
-        display.textContent = userGuess;
+        //display the letter and make it green
+        display.classList.add("correct");
+        display.innerHTML = userGuess;
+        // increase the number of wins by 1 and display new score
         numWins++;
         numberWins.textContent = numWins;
+        // restart the game
         initialiseGame();
     } 
+    // if the user runs out of guesses...
     else if (numGuesses === 1) {
+        // restart the game
         initialiseGame();
+        // increase the number of losses by 1 and displat new score
         numLosses++;
         numberLosses.textContent = numLosses;
     }
+    // if the user chooses the wrong letter...
     else {
+        // tester - DELETE ON DEPLOY
         console.log("Boo!");
+        // add the input to the guessedLetters string
         guessedLetters += userGuess;
+        // display the string on the page
         guessed.textContent = guessedLetters;
+        // decrease the number of guesses by 1 and display
         numGuesses--;
         numberGuesses.textContent = numGuesses;
 
     }
 }
 
-// Game Mechanics
+// GAME MECHANICS
 
+// displays inital wins score of 0
 numberWins.textContent = numWins;
+
+// displays initial losses score of 0
 numberLosses.textContent = numLosses;
+
+// starts the game
 initialiseGame();
 
+// records user input and runs appropriate function
 document.onkeyup = function () {
     matchKey();
 }
